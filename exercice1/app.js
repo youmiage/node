f5b3d670a57313a1c8cf6169d22345074e991f01
youmiage <youmiage44@gmail.com>
2025-10-21 10:24:25 +0100

    Premier commit : ajout des activités Node.js

---
+++ exercice1/app.js
@@ -0,0 +1,10 @@
+const PI = 3.14;
+function calculerAire (rayon){
+    return PI * rayon * rayon;
+}
+console.log(calculeAire(2));
+const PI = 3.14;
+function calculerAire (rayon){
+    return PI * rayon * rayon;
+}
+console.log(calculeAire(2));
---
+++ exercice1/mathA.js
@@ -0,0 +1,5 @@
+const PI = 3.14;
+function calculerAire (rayon){
+    return PI * rayon * rayon;
+}
+console.log(calculeAire(2));
