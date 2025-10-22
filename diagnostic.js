c3fbaf9d1f2c6da3aa29d61310a294476dcc410a
youmiage <youmiage44@gmail.com>
2025-10-22 21:41:11 +0100

    suppression de fichier diagnostic

--- diagnostic.js
+++
@@ -1,8 +0,0 @@
-const os = require("os");
-
-console.log("Plateforme :", os.platform());
-console.log("Architecture :", os.arch());
-console.log("CPU :", os.cpus().length, "cœurs");
-console.log("Mémoire totale :", os.totalmem(), "octets");
-console.log("Mémoire libre :", os.freemem(), "octets");
-console.log("Uptime (en heures) :", (os.uptime() / 3600).toFixed(2));
